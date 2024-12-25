80dfdcb8b9bd932f0936ee8c8b407248cff6947d
Arman Mikoyan <arman.mikoyan@optimumpartners.co>
2024-12-25 20:35:19 +0400

    button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -6,24 +6,41 @@ import decrementIcon from "@/assets/icons/icon-decrement-quantity.svg";
 import decrementHoverIcon from "@/assets/icons/icon-decrement-quantity-hover.svg";
 import { ButtonStyled, IconWrapper } from "./styles";
 import { BUTTON_STYLES } from "./constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Button = ({ children, style = BUTTON_STYLES.PRIMARY }) => {
-  const [isActiveButton, setIsActiveButton] = useState(false);
+  const [selectedItemsCount, setSelectedItemsCount] = useState(0);
   const [decrementSrc, setDecrementSrc] = useState(decrementIcon);
   const [incrementSrc, setIncrementSrc] = useState(incrementIcon);
 
-  const toggleActiveButton = () => {
-    setIsActiveButton((prev) => !prev);
+  useEffect(() => {
+    if (selectedItemsCount === 0) {
+      setDecrementSrc(decrementIcon);
+      setIncrementSrc(incrementIcon);
+    }
+  }, [selectedItemsCount]);
+
+  const selectItem = () => {
+    if (selectedItemsCount === 0) {
+      setSelectedItemsCount(1);
+    }
+  };
+
+  const incrementItem = () => {
+    setSelectedItemsCount((prev) => prev + 1)
+  };
+
+  const decrementItem = () => {
+    setSelectedItemsCount((prev) => prev - 1)
   };
 
   const buttonStyle =
-    isActiveButton && style === BUTTON_STYLES.CART
+  selectedItemsCount && style === BUTTON_STYLES.CART
       ? BUTTON_STYLES.CART_ACTIVE
       : style;
 
   return (
-    <ButtonStyled $style={buttonStyle} onClick={toggleActiveButton}>
+    <ButtonStyled $style={buttonStyle} onClick={selectItem}>
       {buttonStyle === BUTTON_STYLES.CART && (
         <>
           <Image alt="cart" src={cartIcon} /> {children}
@@ -32,17 +49,19 @@ const Button = ({ children, style = BUTTON_STYLES.PRIMARY }) => {
       {buttonStyle === BUTTON_STYLES.CART_ACTIVE && (
         <>
           <IconWrapper
-            alt="decrement quantity"
+            alt="decrement"
             src={decrementSrc}
-            onMouseEnter={() => setDecrementSrc(decrementHoverIcon)}
-            onMouseLeave={() => setDecrementSrc(decrementIcon)}
+            onMouseOver={() => setDecrementSrc(decrementHoverIcon)}
+            onMouseOut={() => setDecrementSrc(decrementIcon)}
+            onClick={decrementItem}
           />
-          1
+          {selectedItemsCount}
           <IconWrapper
-            alt="increment quantity"
+            alt="increment"
             src={incrementSrc}
-            onMouseEnter={() => setIncrementSrc(incrementHoverIcon)}
-            onMouseLeave={() => setIncrementSrc(incrementIcon)}
+            onMouseOver={() => setIncrementSrc(incrementHoverIcon)}
+            onMouseOut={() => setIncrementSrc(incrementIcon)}
+            onClick={incrementItem}
           />
         </>
       )}
